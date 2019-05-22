03ff274beb39f57ea225312f4713d8a79b387cc1
bronson49 <bcharly49@gmail.com>
2019-05-22 15:46:08 +0300

    food start slider

--- app/core/main.js
+++ app/core/main.js
@@ -3,9 +3,11 @@ import slickCarousel from 'slick-carousel/slick/slick.min';
 import {menuFunc} from '../components/menu/menu';
 import {vacancyFunc} from '../components/vacancy/vacancy';
 import {certFunc} from '../components/certif/certif';
+import {eatFunc} from '../components/toeat/toeat';
 
 menuFunc();
 vacancyFunc();
+eatFunc();
 
 certFunc();
 
