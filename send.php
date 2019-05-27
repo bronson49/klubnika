be1a78addbaee8769d147ec43dda5bfbffd3c201
bronson49 <bcharly49@gmail.com>
2019-05-27 12:07:28 +0300

    Updates

---
+++ send.php
@@ -0,0 +1,54 @@
+<?php
+$username = null;
+$usermail = null;
+$userphone = null;
+$comment = null;
+$info = null;
+$sendto = "musevich@gmail.com";
+if(isset($_POST['name']) > 1) {
+    $username = $_POST['name'];
+}
+if(isset($_POST['email']) > 1) {
+    $usermail = $_POST['mail'];
+}
+if(isset($_POST['phone']) > 1) {
+    $userphone = $_POST['phone'];
+}
+if(isset($_POST['messg']) > 1) {
+    $comment = $_POST['messg'];
+}
+if(isset($_POST['info']) > 1) {
+    $info = $_POST['info'];
+}
+// Формирование заголовка письма
+$subject  = "Заявка с сайта Клубника";
+$headers  = "From: klubnika.com.ua\r\n";
+$headers .= "Reply-To: klubnika.com.ua\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+// Формирование тела письма
+$msg  = "<html><body style='font-family:Arial,sans-serif; font-size: 16px;'>";
+$msg .= '<style> td {min-width: 100px; padding: 0 15px;}</style>';
+if($username != null) {
+    $msg .="<div>Имя:".$username."</div>";
+}
+if($usermail != null) {
+    $msg .="<div>Почта:".$usermail."</div>";
+}
+if($userphone != null) {
+    $msg .="<div>Телефон:".$userphone."</div>";
+}
+if($comment != null) {
+    $msg .="<div>Комментарий:".$comment."</div>";
+}
+if($info != null) {
+    $msg .="<div>Описание:".$info."</div>";
+}
+$msg .= "</body></html>";
+// отправка сообщения
+if(@mail($sendto, $subject, $msg, $headers)) {
+    echo 'true';
+} else {
+    echo "false";
+}
+?>
